1ac2e60afed969cc830837351ef3ca104ff16d4c
DanCarson1775 <dancarson1775@gmail.com>
2025-12-10 11:53:44 -0800

    adds main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Part 1
+print("Enter first number: ", end="")
+num1 = int(input())
+print("Enter second number: ", end="")
+num2 = int(input())
+total = num1 + num2
+print(f"Sum of numbers is {total}")
+
+# Part 2
+print("Enter first number: ", end="")
+num1 = int(input())
+print("Enter second number: ", end="")
+num2 = int(input())
+total = num1 * num2
+print(f"Product of numbers is {total}")
